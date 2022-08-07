eb5a0dc047f336c9e7703a80cac6c8ea96910404
Kentaro-Machida <kenkenm120319@gmail.com>
2022-08-07 18:59:43 +0900

    debug test

--- hello_world.py
+++ hello_world.py
@@ -13,8 +13,15 @@ def test_func():
 
 @app.route('/', methods=['GET', 'POST'])
 def hello():
-    name = "Hello World from kochos"
-    return name
+    return '''
+    <!doctype html>
+    <title>Upload new File</title>
+    <h1>Upload new File</h1>
+    <form method=post enctype=multipart/form-data>
+      <input type=file name=file>
+      <input type=submit value=Upload>
+    </form>
+    '''
  
  
 ## おまじない
